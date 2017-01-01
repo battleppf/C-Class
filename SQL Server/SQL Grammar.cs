4448fbeed0a9ebee938269466f27913bad8e2d5b
battleppf <battleppf@sina.com>
2017-01-01 17:16:27 +0800

    Update and rename SQL Grammar.md to SQL Grammar.cs

--- SQL Server/SQL Grammar.cs
+++ SQL Server/SQL Grammar.cs
@@ -1,7 +1,7 @@
 class subjectOperator
 {
         //The main point of this function is the using of the identity_number of sql server
-       ' public void subjectInsert(string name,string type1,string type2,int jige,int lianghao,int youxiu,string parameter)'
+        public void subjectInsert(string name,string type1,string type2,int jige,int lianghao,int youxiu,string parameter)
         {
             string cmd = "set IDENTITY_INSERT B_考核课目目录 on \n";
             cmd += "insert B_考核课目目录(课目编号,课目名称,考核类型,评分类型,大纲及格标准,大纲良好标准,大纲优秀标准,成绩单位) ";
